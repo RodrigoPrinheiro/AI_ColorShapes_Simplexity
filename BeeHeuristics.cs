7df5e26dc381b074deb128ee85899345d4569b57
Rodrigo Pinheiro <rodpinheiro.12@gmail.com>
2020-03-24 15:51:55 +0000

    Testing in program

--- BeeHeuristics.cs
+++ BeeHeuristics.cs
@@ -2,6 +2,7 @@ using System;
 using System.Threading;
 using ColorShapeLinks.Common;
 using ColorShapeLinks.Common.AI;
+using System.Collections.Generic;
 
 namespace BeeAI
 {
@@ -10,23 +11,8 @@ namespace BeeAI
         // Heuristic function
         public static float Heuristic(Board board, PColor color)
         {
-            float beesTotal, enemyTotal = 0;
-            
-            // Distance between two points
-            float Dist(float x1, float y1, float x2, float y2)
-            {
-                return (float) Math.Sqrt(
-                    Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2));
-            }
-
-            // Determine the center row
-            float centerRow = board.rows / 2;
-            float centerCol = board.cols / 2;
-
-            // Maximum points a piece can be awarded when it's at the center
-            float maxPoints = Dist(centerRow, centerCol, 0, 0);
-
-            // Current heuristic value
+            // Max points the ai can hold
+            float maxPoints = 100;
             float h = 0;
 
             // Loop through the board looking for pieces
@@ -40,22 +26,7 @@ namespace BeeAI
                     // Is there any piece there?
                     if (piece.HasValue)
                     {
-                        // If the piece is of our color, increment the
-                        // heuristic inversely to the distance from the center
-                        if (piece.Value.color == color)
-                            h += maxPoints - Dist(centerRow, centerCol, i, j);
-                        // Otherwise decrement the heuristic value using the
-                        // same criteria
-                        else
-                            h -= maxPoints - Dist(centerRow, centerCol, i, j);
-                        // If the piece is of our shape, increment the
-                        // heuristic inversely to the distance from the center
-                        if (piece.Value.shape == color.Shape())
-                            h += maxPoints - Dist(centerRow, centerCol, i, j);
-                        // Otherwise decrement the heuristic value using the
-                        // same criteria
-                        else
-                            h -= maxPoints - Dist(centerRow, centerCol, i, j);
+
                     }
                 }
             }
--- Program.cs
+++ Program.cs
@@ -7,18 +7,33 @@ namespace BeeAI
     {
         static void Main(string[] args)
         {
-            
+            TestHeuristic();
         }
 
         static void TestHeuristic()
         {
             Board board = new Board();
 
-            board.DoMove(PShape.Square, 0);
-            board.DoMove(PShape.Square, 3);
-            board.DoMove(PShape.Round, 3);
+            board.DoMove(PShape.Round, 0);
+            board.DoMove(PShape.Square, 1);
+            board.DoMove(PShape.Round, 2);
             board.DoMove(PShape.Square, 3);
 
+            board.DoMove(PShape.Round, 6);
+            board.DoMove(PShape.Round, 6);
+            board.DoMove(PShape.Round, 6);
+
+            foreach( var v in board.winCorridors)
+            {
+                Console.WriteLine();
+                foreach(var t in v)
+                {
+                    Console.Write($"{t}, ");
+                    Console.WriteLine();
+                }
+            }
+
+            Console.WriteLine(BeeHeuristics.Heuristic(board, PColor.White));
         }
     }
 }
